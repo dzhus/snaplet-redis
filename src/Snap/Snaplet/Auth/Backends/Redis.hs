bca4bea245c661a5ad06dbbcf659cb4d5053bfd2
Aaron Ash <aaron.ash@gmail.com>
2014-06-24 14:04:48 +1000

    Started implementing redisSave

--- src/Snap/Snaplet/Auth/Backends/Redis.hs
+++ src/Snap/Snaplet/Auth/Backends/Redis.hs
@@ -14,9 +14,10 @@ import qualified Data.ByteString.Lazy as LB
 import qualified Data.ByteString as B
 import qualified Data.Map as HM
 import           Data.Map (Map)
-import           Data.Maybe (fromJust, isJust)
+import           Data.Maybe (fromJust, fromMaybe, isJust)
 import           Data.Text (Text)
 import qualified Data.Text as T
+import           Data.Text.Encoding as E
 import           Data.Time
 import           Database.Redis
 import           Web.ClientSession
@@ -64,15 +65,95 @@ data RedisAuthManager = RedisAuthManager {
                       conn :: Connection
                       }
 
-{-AA TODO: implement these functions-}
+{--------------------------------------------------------------------------------}
+{--- | Type representing the concept of a User in your application.-}
+{-data AuthUser = AuthUser-}
+    {-{ userId               :: Maybe UserId-}
+    {-, userLogin            :: Text-}
+
+    {--- We have to have an email field for password reset functionality, but we-}
+    {--- don't want to force users to log in with their email address.-}
+    {-, userEmail            :: Maybe Text-}
+    {-, userPassword         :: Maybe Password-}
+    {-, userActivatedAt      :: Maybe UTCTime-}
+    {-, userSuspendedAt      :: Maybe UTCTime-}
+    {-, userRememberToken    :: Maybe Text-}
+    {-, userLoginCount       :: Int-}
+    {-, userFailedLoginCount :: Int-}
+    {-, userLockedOutUntil   :: Maybe UTCTime-}
+    {-, userCurrentLoginAt   :: Maybe UTCTime-}
+    {-, userLastLoginAt      :: Maybe UTCTime-}
+    {-, userCurrentLoginIp   :: Maybe ByteString-}
+    {-, userLastLoginIp      :: Maybe ByteString-}
+    {-, userCreatedAt        :: Maybe UTCTime-}
+    {-, userUpdatedAt        :: Maybe UTCTime-}
+    {-, userResetToken       :: Maybe Text-}
+    {-, userResetRequestedAt :: Maybe UTCTime-}
+    {-, userRoles            :: [Role]-}
+    {-, userMeta             :: HashMap Text Value-}
+    {-}-}
+  {-deriving (Show,Eq)-}
+
+userHashKey :: Text -> B.ByteString
+{-userHashKey user = B.append (B.fromString "user:") (E.encodeUtf8 user)-}
+userHashKey user = B.append "user:" (E.encodeUtf8 user)
+
+enc = E.encodeUtf8
+
 redisSave :: RedisAuthManager -> AuthUser -> IO (Either AuthFailure AuthUser)
-redisSave r u = undefined
+redisSave r u = 
+    do runRedis (conn r) $ do
+         {- AA TODO: Check if user exists and incr next:userid if not -}
+         res <- multiExec $ do
+           res1 <- hmset (userHashKey $ userLogin u) 
+            [("userId",             (enc $ maybe "" unUid $ userId u)),
+             ("userLogin",          (enc $ userLogin u)),
+             ("userEmail",          (enc $ fromMaybe "" $ userEmail u)),
+             ("userPassword",       (enc $ maybe "" (T.pack . show) $ userPassword u)), {- AA TODO: encrypt as necessary -}
+             ("userActivatedAt",    (enc $ maybe "" (T.pack . show) $ userActivatedAt u)),
+             ("userSuspendedAt",    (enc $ maybe "" (T.pack . show) $ userSuspendedAt u)),
+             ("userRememberToken",          (enc $ fromMaybe "" $ userRememberToken u)),
+    {-{ userId               :: Maybe UserId-}
+    {-, userLogin            :: Text-}
+    {--- We have to have an email field for password reset functionality, but we-}
+    {--- don't want to force users to log in with their email address.-}
+    {-, userEmail            :: Maybe Text-}
+    {-, userPassword         :: Maybe Password-}
+    {-, userActivatedAt      :: Maybe UTCTime-}
+    {-, userSuspendedAt      :: Maybe UTCTime-}
+    {-, userRememberToken    :: Maybe Text-}
+    {-, userLoginCount       :: Int-}
+    {-, userFailedLoginCount :: Int-}
+    {-, userLockedOutUntil   :: Maybe UTCTime-}
+    {-, userCurrentLoginAt   :: Maybe UTCTime-}
+    {-, userLastLoginAt      :: Maybe UTCTime-}
+    {-, userCurrentLoginIp   :: Maybe ByteString-}
+    {-, userLastLoginIp      :: Maybe ByteString-}
+    {-, userCreatedAt        :: Maybe UTCTime-}
+    {-, userUpdatedAt        :: Maybe UTCTime-}
+    {-, userResetToken       :: Maybe Text-}
+    {-, userResetRequestedAt :: Maybe UTCTime-}
+    {-, userRoles            :: [Role]-}
+    {-, userMeta             :: HashMap Text Value-}
+            ("", "")
+            ]
+           {- AA TODO: set "userid:1000" = "bob" -}
+           return $ res1
+         case res of
+           TxSuccess _ -> return $ Right u
+           TxAborted -> return $ Left $ AuthError "redis transaction aborted"
+           TxError e -> return $ Left $ AuthError e
+
+{-AA TODO: implement these functions-}
 redisDestroy :: RedisAuthManager -> AuthUser -> IO ()
 redisDestroy = undefined
+
 redisLookupByUserId :: RedisAuthManager -> UserId -> IO (Maybe AuthUser)
 redisLookupByUserId = undefined
+
 redisLookupByLogin :: RedisAuthManager -> Text -> IO (Maybe AuthUser)
 redisLookupByLogin = undefined
+
 redisLookupByRememberToken :: RedisAuthManager -> Text -> IO (Maybe AuthUser)
 redisLookupByRememberToken = undefined
 
